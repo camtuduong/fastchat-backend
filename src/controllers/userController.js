d3506e917159d3b92f25a72abd5c88d0fb167457
teq-tuduong <tuduong@teqnological.asia>
2026-07-03 15:08:33 +0700

    feat(user): add endpoint to fetch user by ID with error handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -36,3 +36,19 @@ export const findUserBySearch = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId).select(
+      "username displayName avatarUrl",
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,12 +3,14 @@ import {
   getMe,
   updateProfile,
   findUserBySearch,
+  getUserById,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/me", getMe);
 router.get("/all", findUserBySearch);
+router.get("/:userId", getUserById);
 router.patch("/upload", updateProfile);
 
 export default router;
