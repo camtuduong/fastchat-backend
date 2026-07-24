2ee596171a2d19f1fee5ae75765e2ab2f367888f
teq-tuduong <tuduong@teqnological.asia>
2026-07-24 13:06:07 +0700

    refactor(message): consolidate message sending logic and update route to use unified sendMessage function

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,14 +1,12 @@
 import express from "express";
 import {
   deleteMessageWithEveryOne,
-  sendDirectMessage,
-  sendGroupMessage,
+  sendMessage,
 } from "../controllers/messageController.js";
 
 const router = express.Router();
 
-router.post("/direct", sendDirectMessage);
-router.post("/group", sendGroupMessage);
+router.post("/", sendMessage);
 router.delete("/:messageId", deleteMessageWithEveryOne);
 
 export default router;
